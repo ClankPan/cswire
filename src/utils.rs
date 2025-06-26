86ef1d95dc6601b1fe956ac38981d36b330aa7b1
clankpan <clankpan26cm@gmail.com>
2025-06-26 23:01:29 +0900

    add is_zero

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use ark_ff::Field;
 
-use crate::{CSWire, Lin};
+use crate::{CSWire, Lin, extract::ToRaw};
 
 pub fn pow<'a, F: Field>(cs: &'a CSWire<F>, mut base: Lin<'a, F>, mut exp: u64) -> Lin<'a, F> {
     let mut pow = cs.one();
@@ -17,4 +17,21 @@ pub fn pow<'a, F: Field>(cs: &'a CSWire<F>, mut base: Lin<'a, F>, mut exp: u64) 
 
 pub fn enforce_bits<'a, F: Field>(cs: &'a CSWire<F>, bits: &[Lin<'a, F>]) {
     bits.iter().for_each(|b| cs.equal((cs.one() - b) * b, 0));
+}
+
+pub fn is_zero<'a, F: Field>(cs: &'a CSWire<F>, v: &Lin<'a, F>) -> Lin<'a, F> {
+    // v · z = 1 − q
+    // v · q = 0
+    let is_zero: bool = v.raw() == F::ZERO;
+    let q = cs.alloc(is_zero);
+    let z = if is_zero {
+        cs.alloc(0)
+    } else {
+        let inv = v.raw().inverse().unwrap(); // vはzeroではないので逆元はある。
+        cs.alloc(inv)
+    };
+
+    cs.equal(v * z, cs.one() - &q);
+    cs.equal(v * &q, 0);
+    q
 }
